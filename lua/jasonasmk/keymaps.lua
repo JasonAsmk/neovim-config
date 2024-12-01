b7ff402c1550a3d1b1066f1401a866bc283861ad
jasonasmk <jasonasmk@gmail.com>
2024-12-01 20:51:17 +0200

    add completion and fix stuff

--- lua/jasonasmk/keymaps.lua
+++ lua/jasonasmk/keymaps.lua
@@ -3,7 +3,7 @@ local map = vim.keymap.set
 map("n", "<leader>e", "<cmd>Neotree toggle=true<cr>", { desc= "Open Neo Tree" })
 
 local telescope = require('telescope.builtin')
-map('n', '<leader>ff', telescope.find_files, {})
-map('n', '<leader>fg', telescope.live_grep, {})
-map('n', '<leader>fb', telescope.buffers, {})
-map('n', '<leader>fh', telescope.help_tags, {})
+map('n', '<leader>ff', telescope.find_files, { desc = "Find Files"})
+map('n', '<leader>fg', telescope.live_grep, { desc = "Live grep" })
+map('n', '<leader>fb', telescope.buffers, { desc = "Open buffers" })
+map('n', '<leader>fh', telescope.help_tags, { desc = "Help Tags" })
--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,7 +1,6 @@
 return {
   "lewis6991/gitsigns.nvim",
   enabled = vim.fn.executable "git" == 1,
-  event = "User AstroGitFile",
   opts = {
  --   signs = {
  --     add = { text = get_icon "GitSign" },
@@ -13,5 +12,25 @@ return {
  --   },
     worktrees = vim.g.git_worktrees,
   },
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+        { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
+  }
 }
 
