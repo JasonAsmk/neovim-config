ea38d99846598d25d49dabf20754ab12269fe1ff
Iason Asimakopoulos <jasonasmk@gmail.com>
2025-10-31 15:51:04 +0100

    add a small keybind to set diagnostics on quickfix list

--- init.lua
+++ init.lua
@@ -573,6 +573,11 @@ require("lazy").setup({
 					--  the definition of its *type*, not where it was *defined*.
 					map("grt", require("telescope.builtin").lsp_type_definitions, "[G]oto [T]ype Definition")
 
+					-- open diagnostics on quickfix, does not reload automatically
+					map("gre", function()
+						vim.diagnostic.setqflist({})
+					end, "[G]oto [E]rrors")
+
 					-- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
 					---@param client vim.lsp.Client
 					---@param method vim.lsp.protocol.Method
